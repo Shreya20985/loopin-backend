2251a3c654130c6bd0c4ecd06e7cde1b775e41d4
Shreya Bansal <shreyabansal20985@gmail.com>
2026-07-05 01:29:09 +0530

    Add Aiven MySQL connection with SSL

--- connect.js
+++ connect.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+import fs from "fs";
 dotenv.config();
 
 export const db = mysql.createConnection({
@@ -8,4 +9,7 @@ export const db = mysql.createConnection({
   password: process.env.DB_PASSWORD || "Pan1@rakhi",
   database: process.env.DB_NAME || "social",
   port: process.env.DB_PORT || 3306,
+  ssl: process.env.DB_SSL_CA
+    ? { ca: fs.readFileSync(process.env.DB_SSL_CA) }
+    : undefined,
 });
